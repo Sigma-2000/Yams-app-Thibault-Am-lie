951d2f426e621636ce96939738d3159db8083b4b
thibauttaglialatela <thibauttaglialatela@gmail.com>
2024-05-14 16:13:16 +0200

    📦 NEW: create Homepage

---
+++ Yams-Pastries-App/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/admin">Admin</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
