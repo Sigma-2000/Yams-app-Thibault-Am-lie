f6b687e50b40b086c8301f677e603e596e74ad35
thibauttaglialatela <thibauttaglialatela@gmail.com>
2024-05-15 17:28:58 +0200

    show pastries on Homepage

--- Yams-Pastries-App/src/pages/Homepage.jsx
+++ Yams-Pastries-App/src/pages/Homepage.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import "./Homepage.css";
 import { useGetAllPastriesQuery } from "../features/pastriesFeature/pastriesReducer";
-import PastryCard from "../components/PastryCard";
 
 function Homepage() {
   const { data, error, isLoading } = useGetAllPastriesQuery();
@@ -23,17 +22,25 @@ function Homepage() {
         </button>
       </div>
       <h2>Les lots à gagner</h2>
-      <section>
+      <div className="container">
+        <section>
+      <ul>
         {error ? (
           <h5>Oh no, there was an error 😢 {error.message}</h5>
         ) : isLoading ? (
           <h5>Loading...</h5>
         ) : data ? (
           data.map((pastry) => {
-          return <PastryCard imgUrl={pastry.image} name={pastry.name} quantity={pastry.quantity} key={pastry.id}/>;
+            return (
+              <li key={pastry.id}>
+                Nom: {pastry.name} / quantité : {pastry.quantity}
+              </li>
+            );
           })
         ) : null}
-      </section>
+      </ul>
+        </section>
+      </div>
     </>
   );
 }
