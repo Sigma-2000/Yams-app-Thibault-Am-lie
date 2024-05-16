dba36ea22601aae95840c765ba7dd93044330529
Amélie Guyot <amelieguyot7@gmail.com>
2024-05-16 17:18:39 +0200

    modification quantité patisseries hors api

--- Yams-Pastries-App/src/components/Pastries.jsx
+++ Yams-Pastries-App/src/components/Pastries.jsx
@@ -1,11 +1,35 @@
 import { useGetAllPastriesQuery } from "../features/pastriesFeature/pastriesReducer";
-import './Pastries.css'
+import "./Pastries.css";
+import { useEffect, useState } from "react";
 
 function Pastries() {
   const { data, error, isLoading } = useGetAllPastriesQuery();
- 
-  console.log(data);
+  const [pastries, setPastries] = useState([]);
 
+  useEffect(() => {
+    if (data) {
+      setPastries(data);
+    }
+  }, [data]);
+
+  const incrementQuantity = (id) => {
+    const updatedPastries = pastries.map((pastry) => {
+      if (pastry.id === id) {
+        return { ...pastry, quantity: pastry.quantity + 1 };
+      }
+      return pastry;
+    });
+    setPastries(updatedPastries);
+  };
+  const decrementQuantity = (id) => {
+    const updatedPastries = pastries.map((pastry) => {
+      if (pastry.id === id && pastry.quantity > 0) {
+        return { ...pastry, quantity: pastry.quantity - 1 };
+      }
+      return pastry;
+    });
+    setPastries(updatedPastries);
+  };
 
   return (
     <div className="section-pastries">
@@ -16,26 +40,24 @@ function Pastries() {
             <h5>Oh no, there was an error 😢 {error.message}</h5>
           ) : isLoading ? (
             <h5>Loading...</h5>
-          ) : data ? (
-            data.map((pastry) => {
-              return (
-                <li key={pastry.id}>
-                  {pastry.name} / quantité : {pastry.quantity}
-                  <button
-                    className="button-green"
-                    
-                  >
-                    +
-                  </button>
-                  <button
-                    className="button-red"
-                 
-                  >
-                    x
-                  </button>
-                </li>
-              );
-            })
+          ) : pastries.length ? (
+            pastries.map((pastry) => (
+              <li key={pastry.id}>
+                {pastry.name} / quantité : {pastry.quantity}
+                <button
+                  className="button-green"
+                  onClick={() => incrementQuantity(pastry.id)}
+                >
+                  +
+                </button>
+                <button
+                  className="button-red"
+                  onClick={() => decrementQuantity(pastry.id)}
+                >
+                  x
+                </button>
+              </li>
+            ))
           ) : null}
         </ul>
       </div>
