75c8b876c360d84db558e65a34c50a96cfbf1ce6
Amélie Guyot <amelieguyot7@gmail.com>
2024-05-17 15:44:45 +0200

    improve-architecture

--- Yams-Pastries-App/src/pages/Admin.jsx
+++ Yams-Pastries-App/src/pages/Admin.jsx
@@ -1,83 +1,12 @@
-import { useState } from 'react';
-import { useLoginMutation } from '../features/authFeature/authReducer';
-import { useNavigate } from 'react-router-dom'; 
+import Login from '../components/Login';
 import './Admin.css'
 
-//TODO: aurait du mettre ça dans un composant login afin que la page ne gère que l'affichage
 function Admin() {
-  const [email, setEmail] = useState('');
-  const [error, setError] = useState('');
-  const [password, setPassword]= useState('');
-
-  const [login, { isLoading }] = useLoginMutation();
-  const navigate = useNavigate();
-  
-  const validateEmail = (email) => {
-      const emailRegex = /^[^@]+@[^@]+$/;
-      return emailRegex.test(email);
-  };
-/*
-  const validatePassword = (password) => {
-      const passwordRegex = /^(?=.*\d)[a-zA-Z\d]{8,20}$/;
-      return passwordRegex.test(password);
-  };*/
-
-  const handleSubmit = async (e) => {
-      e.preventDefault();
-      setError('');
-
-      if (!validateEmail(email)) {
-          setError('Email invalide.');
-          return;
-      }
-
-     /* if (!validatePassword(password)) {
-          setError('Le mot de passe doit contenir entre 8 et 20 caractères, avec au moins un chiffre.');
-          return;
-      }*/
-      try {
-        const result = await login({ email, password });
-        if (result.data) {
-          console.log('Connexion réussie', result.data);
-          setError('');
-          navigate('/dashboard');
-        } else {
-          setError('Erreur d\'authentification.');
-        }
-      } catch (err) {
-        console.error('Authentication error:', err);
-        setError('Erreur d\'authentification.');
-      }
-    };
 
   return (
-       <div className="login-container">
-          <h2>Connexion</h2>
-          <form onSubmit={handleSubmit}>
-              <div className="form-group">
-                  <label>Email:</label>
-                  <input
-                      type="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      className="form-input"
-                  />
-              </div>
-              <div className="form-group">
-                  <label>Mot de passe:</label>
-                  <input
-                      type="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      className="form-input"
-                  />
-              </div>
-              {error && <p style={{ color: 'red' }}>{error}</p>}
-              <button type="submit" disabled={isLoading}>
-                  {isLoading ? 'Connexion...' : 'Se connecter'}
-              </button>
-          </form>
-      </div>
+    <div>
+      <Login/>
+    </div>
   );
 }
 
